4ea7531e94305a70ba5efc64d282d8f5d1a5ebda
Iconians <claytoncripe@gmail.com>
2022-11-30 20:03:46 -0800

    changed css filename

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "./HomePage.css";
-
+import "./HomePage.module.css";
 import ProductService from "../../services";
 import CategoryPage from "../CategoryPage/CategoryPage";
 import ItemComponent from "../ItemComponent/ItemComponent";
